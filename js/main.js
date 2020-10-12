5cf3d4854899d51612e625216d45b16d75301ce3
git-bugs <voov3r@gmail.com>
2020-10-12 20:49:25 +0300

    lesson3

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
-const num = 266219;
+'use strict';
 
-const str = num.toString();
-let result = 1;
 
-for (let i = 0; i<str.length; i++){
-  result *= str[i];
+let lang = 'en';
+
+if (lang === 'ru'){
+  console.log('Понедельник');
+} else {
+  console.log('Monday');
 };
 
-console.log(result);
+switch (lang){
+  case 'ru':
+    console.log('Понедельник');
+    break;
+  case 'en':
+    console.log('Monday');
+    break;
+};
 
-result **= 3;
 
-console.log(result.toString()[0]);
-console.log(result.toString()[1]);
+let arr = {
+  ru: ['Понедельник','Вторник','Среда'],
+  en: ['Monday','Tuesday','Wednesday']
+};
+console.log(arr[lang]);
+
+
+
+let namePerson = 'Максим';
+let position;
+
+position = (namePerson === 'Артем') ? 'Директор' :
+(namePerson === 'Максим') ? 'Преподаватель' :
+'студент' ;
+
+console.log(position);
