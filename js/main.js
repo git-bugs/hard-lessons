4024dee15cd932dc03f0d8b395cdc6fb242b200f
git-bugs <voov3r@gmail.com>
2020-10-16 21:07:51 +0300

    fx

--- js/main.js
+++ js/main.js
@@ -10,11 +10,9 @@ if (day == -1){ day = 6};
 
 for (let  i=0; i<week.length; i++){
   if ((i == 5 || i == 6) && i !== day){
-    let s = `<span style="font-style: italic;">${week[i]}</span><br>`;
-    document.write(s);
+    document.write(`<span style="font-style: italic;">${week[i]}</span><br>`);
   } else if (i === day){
-    let s = `<span style="font-weight: 700;">${week[i]}</span><br>`;
-    document.write(s);
+    document.write(`<span style="font-weight: 700;">${week[i]}</span><br>`);
   } else {
     document.write(`<span>${week[i]}</span><br>`);
   }
