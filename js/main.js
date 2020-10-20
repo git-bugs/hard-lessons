2bffa74604706993b0a8f8f65f42974ddcb4f40a
git-bugs <voov3r@gmail.com>
2020-10-20 21:53:23 +0300

    hl9

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
 'use strict';
 
 
-let week = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
-let data = new Date();
-let day = data.getDay()-1;
+let week = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+  month = ['Января','Февраля','Марта','Апреля','Мая','Июня','Июля','Августа','Сентября','Октября','Ноября','Декабря'],
+  data = new Date(),
+  minutesWord = 'минут',
+  secondWord = 'секунд',
+  hourWord = 'час';
 
-if (day == -1){ day = 6};
 
-
-for (let  i=0; i<week.length; i++){
-  if ((i == 5 || i == 6) && i !== day){
-    document.write(`<span style="font-style: italic;">${week[i]}</span><br>`);
-  } else if (i === day){
-    document.write(`<span style="font-weight: 700;">${week[i]}</span><br>`);
+let hourChange = function(x){
+  if (x == 1 || x.toString()[1] == 1) {
+    return 'час'
+  } else if (x > 1 && x < 5 || x > 21){
+    return 'часа'
   } else {
-    document.write(`<span>${week[i]}</span><br>`);
+    return 'часов'
   }
-}
+};
+
+
+let secondsChange = function(x){
+  if (x > 10 && x < 20){
+    return ''
+  } else if(x.toString()[1] == 1 || x.toString() == 1){
+    return 'a'
+  } else if(x.toString()[1] > 4 || (x.toString() > 4 && x.toString() <= 10) || (x.toString()[1] == 0 || x.toString() == 0)){
+    return ''
+  } else {
+    return 'ы'
+  }
+};
+
+
+setInterval(function(){
+  data = new Date();
+  let time = ['Сегодня' + ' ' + week[data.getDay()], ' ' + data.getDate() + ' ' + month[data.getMonth()] + ' ' +  data.getFullYear() + ' ' + 'года', ' ' + data.getHours() + ' ' + hourChange(data.getHours())  + ' ' + data.getMinutes() + ' ' +  minutesWord + secondsChange(data.getMinutes())  + ' ' +  data.getSeconds()  + ' ' +  secondWord + secondsChange(data.getSeconds())].join(',');
+  document.querySelector('h1').innerHTML = time;
+},1000);
+
+
+let zeroCheck = function(x){
+  return x < 10? '0' + x: x;
+};
+
+setInterval(function(){
+  data = new Date();
+  let time = [zeroCheck(data.getDate()) + '.' + zeroCheck(data.getMonth()+1) + '.' + data.getFullYear() + ' - ' + zeroCheck(data.getHours()) + ':' + zeroCheck(data.getMinutes()) + ':' + zeroCheck(data.getSeconds())].join('');
+  document.querySelector('h2').innerHTML = time;
+},1000);
