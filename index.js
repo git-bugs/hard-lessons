f45858d847d3d1f768ff705b044341f302fd5a3c
git-bugs <voov3r@gmail.com>
2020-11-21 08:18:24 +0300

    fix

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const selectCities = document.getElementById('select-cities'),
   input = document.getElementById('select-cities'),
   label = document.querySelector('.label'),
   closeBtn = document.querySelector('.close-button'),
-  linkBtn = document.querySelector('.button');
+  linkBtn = document.querySelector('.button')
+
+let data = {};
 
 const getData = () => {
   return fetch('./db_cities.json')
@@ -16,22 +18,23 @@ const getData = () => {
       if (response.status !== 200) {
         throw new Error('error')
       }
-      return (response.json());
+      defaultList.textContent = '';
+      return response.json();
     })
+    .then(temp => data = { ...temp })
     .catch((error) => console.error(error))
 };
+getData();
 
 const getDefaultList = () => {
-  getData()
-    .then(data => {
-      for (let item in data) {
-        data[item].forEach((elem, index) => {
-          elem.cities.sort(function (a, b) {
-            return b.count - a.count;
-          });
-          const countryBlock = document.createElement('div');
-          countryBlock.classList.add('dropdown-lists__countryBlock');
-          countryBlock.innerHTML = `
+  for (let item in data) {
+    data[item].forEach((elem, index) => {
+      elem.cities.sort(function (a, b) {
+        return b.count - a.count;
+      });
+      const countryBlock = document.createElement('div');
+      countryBlock.classList.add('dropdown-lists__countryBlock');
+      countryBlock.innerHTML = `
         <div class="dropdown-lists__total-line">
           <div class="dropdown-lists__country">${data[item][index].country}</div>
           <div class="dropdown-lists__count">${data[item][index].count}</div>
@@ -48,94 +51,83 @@ const getDefaultList = () => {
           <div class="dropdown-lists__city">${elem.cities[2].name}</div>
           <div class="dropdown-lists__count">${elem.cities[2].count}</div>
         </div>`;
-          defaultList.appendChild(countryBlock);
-        });
-      }
-    })
+      defaultList.appendChild(countryBlock);
+    });
+  }
 };
 
 const getCities = (target) => {
   const country = target.querySelector('.dropdown-lists__country').textContent;
   input.value = country;
-
-  getData()
-    .then(data => {
-      for (let item in data) {
-        data[item].forEach((elem) => {
-          if (elem.country === country) {
-            dropDown.scrollTop = 0;
-            const countryBlock = document.createElement('div');
-            countryBlock.classList.add('dropdown-lists__countryBlock');
-            countryBlock.innerHTML = `
+  for (let item in data) {
+    data[item].forEach((elem) => {
+      if (elem.country === country) {
+        dropDown.scrollTop = 0;
+        const countryBlock = document.createElement('div');
+        countryBlock.classList.add('dropdown-lists__countryBlock');
+        countryBlock.innerHTML = `
             <div class="dropdown-lists__total-line">
               <div class="dropdown-lists__country">${elem.country}</div>
               <div class="dropdown-lists__count">${elem.count}</div>
             </div>`;
-            selectList.append(countryBlock);
-            elem.cities.forEach(item => {
-              const cityBlock = document.createElement('div');
-              cityBlock.classList.add('dropdown-lists__line');
-              cityBlock.innerHTML = `
+        selectList.append(countryBlock);
+        elem.cities.forEach(item => {
+          const cityBlock = document.createElement('div');
+          cityBlock.classList.add('dropdown-lists__line');
+          cityBlock.innerHTML = `
             <div class="dropdown-lists__city">${item.name}</div>
             <div class="dropdown-lists__count">${item.count}</div>`;
-              countryBlock.append(cityBlock);
-            });
-          }
+          countryBlock.append(cityBlock);
         });
       }
-      selectList.style.display = 'block';
-      let count = 0;
-      const animate = () => {
-        selectList.style.width = count + '%';
-        count++;
-        if(count === 50) clearInterval(id);
-      };
-      const id = setInterval(animate,5);
     });
+  }
+  selectList.style.display = 'block';
+  let count = 0;
+  const animate = () => {
+    selectList.style.width = count + '%';
+    count++;
+    if (count === 50) clearInterval(id);
+  };
+  const id = setInterval(animate, 5);
 };
 
 const getCityLink = (target) => {
-  getData()
-    .then(data => {
-      for (let item in data) {
-        data[item].forEach((elem) => {
-          const { cities } = elem;
-          cities.forEach(item => {
-            if (item.name === target) {
-              linkBtn.disabled = false;
-              linkBtn.href = item.link;
-            }
-          })
-        })
-      }
-    });
+  for (let item in data) {
+    data[item].forEach((elem) => {
+      const { cities } = elem;
+      cities.forEach(item => {
+        if (item.name === target) {
+          linkBtn.disabled = false;
+          linkBtn.href = item.link;
+        }
+      })
+    })
+  }
 };
 
 const searchCity = (reg) => {
-  getData()
-    .then(data => {
-      for (let item in data) {
-        data[item].forEach((elem) => {
-          elem.cities.forEach(item => {
-            if (item.name.match(reg)) {
-              const cityBlock = document.createElement('div');
-              cityBlock.classList.add('dropdown-lists__line');
-              cityBlock.innerHTML = `
+  for (let item in data) {
+    data[item].forEach((elem) => {
+      elem.cities.forEach(item => {
+        if (item.name.match(reg)) {
+          const cityBlock = document.createElement('div');
+          cityBlock.classList.add('dropdown-lists__line');
+          cityBlock.innerHTML = `
                 <div class="dropdown-lists__city">${item.name}</div>
                 <div class="dropdown-lists__count">${elem.country}</div>`;
-              autocompleteList.append(cityBlock);
-            }
-          });
-        });
-      }
-      if (!autocompleteList.textContent) {
-        const cityBlock = document.createElement('div');
-        cityBlock.classList.add('dropdown-lists__line');
-        cityBlock.innerHTML = `
-          <div class="dropdown-lists__city">Ничего не найдено</div>`;
-        autocompleteList.append(cityBlock);
-      }
+          autocompleteList.append(cityBlock);
+        }
+      });
     });
+  }
+  if (!autocompleteList.textContent) {
+    const cityBlock = document.createElement('div');
+    cityBlock.classList.add('dropdown-lists__line');
+    cityBlock.innerHTML = `
+          <div class="dropdown-lists__city">Ничего не найдено</div>`;
+    autocompleteList.append(cityBlock);
+  }
 };
 
 const listBackAnimate = () => {
@@ -143,9 +135,9 @@ const listBackAnimate = () => {
   const animateList = () => {
     selectList.style.width = count + '%';
     count--;
-    if(count === 0) clearInterval(id);
+    if (count === 0) clearInterval(id);
   };
-  const id = setInterval(animateList,5);
+  const id = setInterval(animateList, 5);
 };
 
 selectCities.addEventListener('blur', () => {
@@ -164,7 +156,7 @@ document.body.addEventListener('click', (event) => {
     getCities(target.closest('.dropdown-lists__total-line'));
   } else if (target.closest('.dropdown-lists__list--select') && target.closest('.dropdown-lists__total-line')) {
     listBackAnimate();
-    
+
   } else if (target.classList.contains('dropdown-lists__country') || target.classList.contains('dropdown-lists__city')) {
     label.style.display = 'none';
     closeBtn.style.display = 'block';
